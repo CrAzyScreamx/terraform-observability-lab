3f48c9f17781818ac0241c88bcc7fcec31fa25b2
CrAzyScreamx <34142944+CrAzyScreamx@users.noreply.github.com>
2025-07-11 19:39:25 +0300

    Update Cloudflare configurations and add server setup script; modify access policy names and add device custom profile resource

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_zero_trust_access_policy" "main" {
   count      = var.cloudflare_configuration ? 1 : 0
   account_id = var.cloudflare_account_id
-  name       = "Allow Zero Trust Access"
+  name       = "Access Policy for ${var.environment_name}-${var.application_name}"
   decision   = "allow"
   include = [{
   everyone = {} }]
@@ -11,7 +11,7 @@ resource "cloudflare_zero_trust_access_policy" "main" {
 resource "cloudflare_zero_trust_tunnel_cloudflared" "main" {
   count      = var.cloudflare_configuration ? 1 : 0
   account_id = var.cloudflare_account_id
-  name       = "Azure Tunnel (${var.environment_name}-${var.application_name})"
+  name       = "Tunnel Managed by Terraform (${var.environment_name}-${var.application_name})"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_route" "main" {
@@ -25,5 +25,22 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "main" {
   count      = var.cloudflare_configuration ? 1 : 0
   account_id = var.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.main[0].id
+}
+
+resource "cloudflare_zero_trust_device_custom_profile" "main" {
+  for_each         = var.cloudflare_configuration ? toset(var.subnet_address_prefixes) : []
+  account_id       = var.cloudflare_account_id
+  name             = "Device Profile - Observability Lab"
+  description      = "Managed by Terraform (${var.environment_name}-${var.application_name})"
+  enabled          = true
+  precedence       = 1
+  allowed_to_leave = true
+  switch_locked    = false
+
+  match = "os.name in {\"windows\" \"mac\" \"linux\"}"
+
+  include = [{
+    address = each.value
+  }]
 }
 #### Cloudflare ####
---
+++ terraform/setupFiles/server.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+apt update
+apt install -y software-properties-common git
+apt-add-repository --yes --update ppa:ansible/ansible || true
+apt install -y ansible dos2unix
+
+
+mkdir -p /opt/bootstrap
+cd /opt/bootstrap
+echo "New"
+echo "${github_key}" | base64 -d > /opt/bootstrap/github.key
+chmod 600 /opt/bootstrap/github.key
+
+dos2unix /opt/bootstrap/github.key
+
+cat << 'EOF' >> ~/.ssh/config
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile /opt/bootstrap/github.key
+  IdentitiesOnly yes
+EOF
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+echo "Running Ansible Pull"
+
+ansible-pull -U git@github.com:CrAzyScreamx/terraform-observability-lab.git ansible/main_playbook.yml --directory=/opt/bootstrap/ansible --checkout=main -i localhost \
+--extra-vars "cloudflare=${cloudflare} tunnel_token=${tunnel_token}"
+
